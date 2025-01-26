410e2ab14d547f8b552b7271f62d68a8ed0e888d
fahmy-dev <faahmy@proton.me>
2025-01-26 15:30:45 +0300

    initial commit

--- src/pages/Directors.jsx
+++ src/pages/Directors.jsx
@@ -1,13 +1,38 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Directors() {
+  const [directors, setDirectors] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:4000/directors`)
+        .then(r => r.json())
+        .then(data => setDirectors(data))
+        .catch(error => console.error(error))
+      }, []);
+
+      if(directors.length === 0){
+        return <h1>Loading...</h1>;
+      };
+  
+  
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        {directors.map((director) => (
+          <article key={director.id}>
+            <h2>{director.name}</h2>
+            <ul>
+              {director.movies.map((movie, index) => (
+                <li key={index}>{movie}</li>
+              ))}
+            </ul>
+          </article>
+        ))}
       </main>
     </>
   );
